ee93bffce1999257b6eb37f2aaeab176f1de94e3
victorr <kolawoledamilola06@gmail.com>
2025-04-25 01:50:46 -0400

    minor UI changes/added characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Chat from "./Chat";
 import Message from "./Message";
@@ -8,20 +8,43 @@ const ai = new GoogleGenAI({
   apiKey: "",
 });
 
-const newChat = ai.chats.create({
-  model: "gemini-2.0-flash",
-  history: [],
-  // contents: chat,
-  config: {
-    systemInstruction: "Try to use emojis.",
-  },
-});
-
 function App() {
   //collect the user chat input
   const [chat, setChat] = useState("");
   // send a request to the Gemini API
   const [aiReply, setAiReply] = useState("");
+  const [selectedCharacter, setSelectedCharacter] = useState("Default Normal");
+  const [newChat, setChatSession] = useState(
+    ai.chats.create({
+      model: "gemini-2.0-flash",
+      history: [],
+      // contents: chat,
+      config: {
+        systemInstruction: "you are a " + selectedCharacter + ".",
+      },
+    })
+  );
+
+  useEffect(() => {
+    setChatSession(
+      ai.chats.create({
+        model: "gemini-2.0-flash",
+        history: [],
+        // contents: chat,
+        config: {
+          systemInstruction: "you are a " + selectedCharacter + ".",
+        },
+      })
+    );
+  }, [selectedCharacter]);
+  // const newChat = ai.chats.create({
+  //   model: "gemini-2.0-flash",
+  //   history: [],
+  //   // contents: chat,
+  //   config: {
+  //     systemInstruction: "you are a medieval knight ",
+  //   },
+  // });
 
   async function submit() {
     const response = await newChat.sendMessage({
@@ -36,11 +59,41 @@ function App() {
   }
 
   return (
-    <div className="App min-h-screen flex flex-col items-center bg-[#0C3B2E]">
+    <div className="App min-h-screen flex flex-col items-center bg-[#0C3B2E] px-5">
       <h1 className="fixed w-full flex justify-center p-4 bg-[#0C3B2E] text-white text-2xl">
         Gemini Ai Chatbot🌟
       </h1>
-      <Message messages={newChat.getHistory()}></Message>
+      <div className="flex mt-20 mb-50 justify-between w-full">
+        <Message messages={newChat.getHistory()}></Message>
+
+        <div>
+          <p className="text-white">Choose A Character!</p>
+          <select
+            name="characters"
+            id="characters"
+            className="bg-gray-600 rounded-2xl p-2 m-2 text-white max-h-15 hover:border-4 "
+            defaultValue="Default(Normal)"
+            onChange={(e) => {
+              setSelectedCharacter(e.target.value);
+            }}
+          >
+            <option value="Default(Normal)">Default (Normal)</option>
+            <option value="Donald Duck">Donald Duck</option>
+            <option value="Mickey Mouse">Mickey Mouse</option>
+            <option value="a Medieval Knight">Knight</option>
+            <option value="Therapist">Therapist</option>
+            <option value="a Pirate">Pirate</option>
+            <option value="a Genie">Genie</option>
+            <option value="a Robot">Robot</option>
+            <option value="a Vampire">Vampire</option>
+            <option value="a Ghost">Ghost</option>
+            <option value="a Wizard">Wizard</option>
+            <option value="a Superhero">Superhero</option>
+            <option value="a Detective">Detective</option>
+            <option value="a Chef">Chef</option>
+          </select>
+        </div>
+      </div>
       <Chat onChatChange={setChat} onChatSubmit={submit} />
     </div>
   );
--- src/Message.tsx
+++ src/Message.tsx
@@ -8,28 +8,25 @@ type MessageProps = {
 
 const Message = ({ messages }: MessageProps) => {
   return (
-    <div className="flex flex-col bg-[#707d7d] rounded-2xl p-2 mb-50 mt-20">
+    <div className="flex flex-col bg-[#707d7d] rounded-2xl p-2  ">
       <h2 className="text-2xl text-white text-center m-2">Messages</h2>
 
       {messages.map((item, index) =>
         item.role == "user" ? (
-          <div className="flex max-w-full items-end">
+          <div className="flex max-w-full items-end" key={index}>
             <p className="m-2 p-2 items-center justify-center text-white">
               You:
             </p>
-            <div
-              className=" m-2 bg-[#00df81] px-4 py-2 rounded-2xl"
-              key={index}
-            >
+            <div className=" m-2 bg-[#00df81] px-4 py-2 rounded-2xl">
               <ReactMarkdown>{item.parts && item.parts[0].text}</ReactMarkdown>
             </div>
           </div>
         ) : (
-          <div className="flex">
+          <div className="flex" key={index}>
             <p className="m-2 p-2 items-center justify-center text-white">
               AI:
             </p>
-            <div className="m-2 bg-[#aacbc4] px-4 py-2 rounded-2xl" key={index}>
+            <div className="m-2 bg-[#aacbc4] px-4 py-2 rounded-2xl">
               <ReactMarkdown>{item.parts && item.parts[0].text}</ReactMarkdown>
             </div>
           </div>
